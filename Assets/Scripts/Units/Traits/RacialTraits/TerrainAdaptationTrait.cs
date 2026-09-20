991d8b12f326bdec5282741c6332ee3ccd648315
kso1223 <gyd0625@gmail.com>
2026-09-20 19:05:31 +0900

    클래스 명 오타 수정, 사용하지 않는 클래스 삭제

--- Assets/Scripts/Units/Traits/RacialTraits/TerrainAdaptationTrait.cs
+++ Assets/Scripts/Units/Traits/RacialTraits/TerrainAdaptationTrait.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// 지정된 지형(예: 산)의 이동 비용을 할인해준다.
 /// </summary>
 [CreateAssetMenu(fileName = "TerrainAffinity", menuName = "Strategy/Traits/Terrain Affinity")]
-public class TerrainAffinityTrait : RacialTrait
+public class TerrainAdaptationTrait : RacialTrait
 {
     [Tooltip("할인이 적용될 지형 (예: TileType_Mountain 에셋)")]
     public TileTypeData targetTileType;
